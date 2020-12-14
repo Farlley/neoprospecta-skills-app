f40f0d5af9c77f80fded0211d9a1e6e7d1755402
Farlley Barbosa <farlley.barbosa@gmail.com>
2020-12-14 11:33:46 -0300

    Add list and detail router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
+import { CustomerListComponent } from './customers/customer-list/customer-list.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: CustomerListComponent
+  },
+  {
+    path: 'customer/:customerId',
+    component: CustomerListComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
