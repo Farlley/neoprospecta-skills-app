63c49538f36f9146f73a5388d95c771128b18c59
Farlley Barbosa <farlley.barbosa@gmail.com>
2020-12-14 10:01:26 -0300

    Add API url environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'http://private-92a969-processoseletivo1.apiary-mock.com'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiUrl: 'http://private-92a969-processoseletivo1.apiary-mock.com'
 };
 
 /*
