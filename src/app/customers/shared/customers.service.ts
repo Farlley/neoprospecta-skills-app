dac1c2a023e704e66a2fb9bf3b5f8cff732e0a10
Farlley Barbosa <farlley.barbosa@gmail.com>
2020-12-14 10:01:38 -0300

    Get customers service

--- src/app/customers/shared/customers.service.ts
+++ src/app/customers/shared/customers.service.ts
@@ -1,9 +1,18 @@
+import { Customer } from './customers.model';
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from './../../../environments/environment';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CustomersService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getCustomers(): Observable<Customer[]> {
+    return this.http.get<Customer[]>(`${environment.apiUrl}/customers`)
+      .pipe();
+  }
 }
