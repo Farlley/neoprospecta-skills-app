062ccadd535932c21527cb42a3fcc53116a9cb57
Farlley Barbosa <farlley.barbosa@gmail.com>
2020-12-15 17:02:35 -0300

    Customers filter

--- src/app/customers/customer-list/customer-list.component.ts
+++ src/app/customers/customer-list/customer-list.component.ts
@@ -2,7 +2,6 @@ import { Customer } from './../shared/customers.model';
 import { CustomersService } from './../shared/customers.service';
 import { Component, OnInit, ɵCompiler_compileModuleSync__POST_R3__ } from '@angular/core';
 import { Router } from '@angular/router';
-import { first } from 'rxjs/operators';
 
 @Component({
   selector: 'app-customer-list',
@@ -11,51 +10,69 @@ import { first } from 'rxjs/operators';
 })
 export class CustomerListComponent implements OnInit {
 
-  customers: Customer[] = []
+  customers: Customer[] = null
+  customersRaw: Customer[] = []
+  filter: string = ''
   limit: number = 10
   pagination: any = this.setPagination(this.customers)
 
   constructor(private customersService: CustomersService, private router: Router) { }
 
   ngOnInit(): void {
-    this.loadCustomers()
-  }
-
-  public loadCustomers(page?: number) {
+    // this.loadCustomers()
     this.customersService.getCustomers()
       .subscribe(
         res => {
-          this.customers = res.slice(page ? page - 1 : 0, 10)          
-          this.pagination = this.setPagination(res, page)
+          this.customersRaw = res
+          this.customers = res
+          this.pagination = this.setPagination(this.customersRaw, 1)
         }
       )
   }
 
-  private setPagination(data: Customer[], current: number = 1, limit: number = 10) {
-    let pages = () => Math.ceil(data.length / limit)
-    return {
-      current: current,
-      limit: limit,
-      pages: pages(),
-      isFirst: current === 1,
-      isLast: current >= pages()
+  public loadCustomers(page?: number) {
+    this.pagination = this.setPagination(this.customers, page)
+  }
+
+  private setPagination(data: Customer[], current: number = 1, limit: number = 10): any {
+    if (data) {
+      let pages = () => Math.ceil(data.length / limit)
+      let page = current === undefined ? 1 : current
+      return {
+        start: limit * (page - 1),
+        end: limit * page,
+        current: current,
+        limit: limit,
+        pages: pages(),
+        isFirst: current === 1,
+        isLast: current >= pages()
+      }
     }
   }
 
-  public nextPage() {
+  public nextPage(): void {
     if (!this.pagination.isLast) {
       this.loadCustomers(this.pagination.current + 1)
     }
   }
 
-  public previousPage() {
+  public previousPage(): void {
     if (!this.pagination.isFirst) {
       this.loadCustomers(this.pagination.current - 1)
     }
   }
 
-  public openCustomer(customer: Customer) {
+  public openCustomer(customer: Customer): void {
     this.router.navigateByUrl(`/customer/${customer.id}`)
+  }
+
+  public filterCustomers(value: string): void {
+    if (value && this.filter) {
+      this.customers = this.customers.filter(x => Object.values(x).map(x => x.toString().toLowerCase()).find(x => x.includes(this.filter.toLowerCase())))
+    } else {
+      this.customers = this.customersRaw
+    }
+    this.loadCustomers(1)
   }
 
 }
